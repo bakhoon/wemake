f90b37dcd304c795aac28728c7bd5811df7b584f
bakhoon <joon7853@gmail.com>
2026-01-07 11:03:12 -0500

    Implement initial application structure with new components and routing. Remove Dockerfile and .dockerignore. Add App component, component example, and various UI components including buttons, inputs, and dropdowns. Update package.json and package-lock.json with new dependencies. Create routes for home and about pages.

---
+++ app/potato/tomato.tsx
@@ -0,0 +1,14 @@
+
+
+export default function AboutUs() {
+    return <div>About Us!</div>;
+}
+
+export const links = () => [
+    { rel: "stylesheet", href: "styles.css" },
+];
+
+export const meta = () => [
+    { title: "Tomato" },
+    { name: "description", content: "Learn more about us Tomato Page" },
+];
